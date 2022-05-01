2a19cbf390c70eb7523aa13d1b481f367b45036d
王禹诺 <1216129807@qq.com>
2022-05-01 14:50:23 +0800

    init

---
+++ src/main/java/com/group2022103/flightkiosk/controller/PlaneController.java
@@ -0,0 +1,9 @@
+package com.group2022103.flightkiosk.controller;
+
+import com.group2022103.flightkiosk.mapper.PlaneMapper;
+
+public class PlaneController {
+	
+	private PlaneMapper planeMapper;
+
+}
---
+++ src/main/java/com/group2022103/flightkiosk/controller/TicketController.java
@@ -0,0 +1,9 @@
+package com.group2022103.flightkiosk.controller;
+
+import com.group2022103.flightkiosk.mapper.TicketMapper;
+
+public class TicketController {
+	
+	private TicketMapper ticketMapper;
+
+}
