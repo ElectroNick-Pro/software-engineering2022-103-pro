f8b477436742eccb9edd4444022994efc8181703
王禹诺 <1216129807@qq.com>
2022-05-11 21:21:33 +0800

    customer controller

--- src/main/java/com/group2022103/flightkiosk/controller/CustomerController.java
+++ src/main/java/com/group2022103/flightkiosk/controller/CustomerController.java
@@ -12,10 +12,14 @@ public class CustomerController {
 	public CustomerFront get(CustomerBack req) {
 		var res = new CustomerFront();
 		Customer customer = null;
-		for(var c: customerMapper.queryAll()) {
-			if(c.getCustomerId().equals(req.getCustomerID())) {
-				customer = c;
-				break;
+		if(req.getID() > 0) {
+			customer = customerMapper.getById(res.getID());
+		} else if(req.getCustomerID() != null) {
+			for(var c: customerMapper.queryAll()) {
+				if(c.getCustomerId().equals(req.getCustomerID())) {
+					customer = c;
+					break;
+				}
 			}
 		}
 		if(customer == null) {
@@ -24,6 +28,9 @@ public class CustomerController {
 		res.setCreditID(customer.getCreditId());
 		res.setPassword(customer.getPassword());
 		res.setSurname(customer.getSurname());
+		res.setCustomerID(customer.getCustomerId());
+		res.setFirstName(customer.getFirstname());
+		res.setID(customer.getId());
 		return res;
 	}
 
