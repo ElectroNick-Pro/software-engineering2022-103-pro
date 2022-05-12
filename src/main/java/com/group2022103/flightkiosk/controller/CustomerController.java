8b4df99f8d2b4705fd164a6238aec503fd3016cf
王禹诺 <1216129807@qq.com>
2022-05-12 20:28:13 +0800

    customer controller

--- src/main/java/com/group2022103/flightkiosk/controller/CustomerController.java
+++ src/main/java/com/group2022103/flightkiosk/controller/CustomerController.java
@@ -2,18 +2,20 @@ package com.group2022103.flightkiosk.controller;
 
 import com.group2022103.flightkiosk.application.Application;
 import com.group2022103.flightkiosk.mapper.CustomerMapper;
+import com.group2022103.flightkiosk.mapper.TicketMapper;
 import com.group2022103.flightkiosk.model.Customer;
 import com.group2022103.flightkiosk.vo.*;
 
 public class CustomerController {
 	
 	private CustomerMapper customerMapper = (CustomerMapper) Application.context.getMapperConfig().getMappers().get(CustomerMapper.class);
+	private TicketMapper ticketMapper = (TicketMapper) Application.context.getMapperConfig().getMappers().get(TicketMapper.class);
 	
 	public CustomerFront get(CustomerBack req) {
 		var res = new CustomerFront();
 		Customer customer = null;
 		if(req.getID() > 0) {
-			customer = customerMapper.getById(res.getID());
+			customer = customerMapper.getById(req.getID());
 		} else if(req.getCustomerID() != null) {
 			for(var c: customerMapper.queryAll()) {
 				if(c.getCustomerId().equals(req.getCustomerID())) {
@@ -21,16 +23,18 @@ public class CustomerController {
 					break;
 				}
 			}
+		} else if(req.getBookingID() != null) {
+			for(var t: ticketMapper.queryAll()) {
+				if(t.getBookingId().equals(req.getBookingID())) {
+					customer = customerMapper.getById(t.getCustomer());
+					break;
+				}
+			}
 		}
 		if(customer == null) {
 			return null;
 		}
-		res.setCreditID(customer.getCreditId());
-		res.setPassword(customer.getPassword());
-		res.setSurname(customer.getSurname());
-		res.setCustomerID(customer.getCustomerId());
-		res.setFirstName(customer.getFirstname());
-		res.setID(customer.getId());
+		res.setCustomer(customer);
 		return res;
 	}
 
