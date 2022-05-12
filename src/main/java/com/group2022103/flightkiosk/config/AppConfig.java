fd5d1c0a0de9f93e6511f03a7df64e559d84ee0e
王禹诺 <1216129807@qq.com>
2022-05-12 22:25:14 +0800

    app config

--- src/main/java/com/group2022103/flightkiosk/config/AppConfig.java
+++ src/main/java/com/group2022103/flightkiosk/config/AppConfig.java
@@ -1,22 +1,33 @@
 package com.group2022103.flightkiosk.config;
 
-import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
 public class AppConfig {
 	
 	private SimpleDateFormat timezone;
+	private MessageDigest digest;
 	
 	public AppConfig() {
 		
 		timezone = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		timezone.setTimeZone(TimeZone.getTimeZone("UTC"));
 		
+		try {
+			digest = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public SimpleDateFormat getTimezone() {
 		return timezone;
+	}
+
+	public MessageDigest getDigest() {
+		return digest;
 	}
 
 }
