b7ed9f6467ad3088767c1c4be9593bfe8fe80503
王禹诺 <1216129807@qq.com>
2022-05-12 22:25:30 +0800

    ticket controller

--- src/main/java/com/group2022103/flightkiosk/controller/TicketController.java
+++ src/main/java/com/group2022103/flightkiosk/controller/TicketController.java
@@ -1,10 +1,13 @@
 package com.group2022103.flightkiosk.controller;
 
-import java.util.ArrayList;
+import java.time.Duration;
+import java.util.*;
 
 import com.group2022103.flightkiosk.application.Application;
 import com.group2022103.flightkiosk.mapper.CustomerMapper;
+import com.group2022103.flightkiosk.mapper.IntervalMapper;
 import com.group2022103.flightkiosk.mapper.TicketMapper;
+import com.group2022103.flightkiosk.model.Interval;
 import com.group2022103.flightkiosk.vo.TicketBack;
 import com.group2022103.flightkiosk.vo.TicketFront;
 
@@ -12,6 +15,7 @@ public class TicketController {
 	
 	private TicketMapper ticketMapper = (TicketMapper) Application.context.getMapperConfig().getMappers().get(TicketMapper.class);
 	private CustomerMapper customerMapper = (CustomerMapper) Application.context.getMapperConfig().getMappers().get(CustomerMapper.class);
+	private IntervalMapper intervalMapper = (IntervalMapper) Application.context.getMapperConfig().getMappers().get(IntervalMapper.class);
 	
 	public TicketFront get(TicketBack req) {
 		var res = new TicketFront();
@@ -22,15 +26,40 @@ public class TicketController {
 					res.getTickets().add(e);
 				}
 			});
-			return res;
-		}
-		if(req.getBookingID() != null) {
+		} else if(req.getBookingID() != null) {
 			ticketMapper.queryAll().forEach((e)->{
 				if(e.getBookingId().equals(req.getBookingID())) {
 					res.getTickets().add(e);
 				}
 			});
 		}
+		
+		var intervalMap = new HashMap<Integer, List<Interval>>(); // FilghtId-Intervals
+		for(var interval : intervalMapper.queryAll()) {
+			var flightId = interval.getFlight();
+			if(!intervalMap.containsKey(flightId)) {
+				intervalMap.put(flightId, new ArrayList<>());
+			}
+			intervalMap.get(flightId).add(interval);
+		}
+		for(var e: intervalMap.entrySet()) {
+			e.getValue().sort((x,y) -> {
+				return x.getDepartureTime().compareTo(y.getDepartureTime());
+			});
+		}
+		res.getTickets().sort((x,y)-> {
+			var xDep = intervalMap.get(x.getFlight()).get(0).getDepartureTime();
+			var yDep = intervalMap.get(y.getFlight()).get(0).getDepartureTime();
+			var now = new Date();
+			// var now = (Date)Application.context.getCsvConfig().getParseMethodMap().get(Date.class).apply("2022-04-03 00:00:00");
+			if(xDep.after(now) ^ yDep.after(now)) {
+				return yDep.compareTo(xDep);
+			} else {
+				return Duration.between(xDep.toInstant(), now.toInstant()).abs()
+					.compareTo(Duration.between(yDep.toInstant(), now.toInstant()));
+			}
+		});
+		
 		return res;
 	}
 
---
+++ src/test/java/com/group2022103/flightkiosk/controllerTest/TicketControllerTest.java
@@ -0,0 +1,32 @@
+package com.group2022103.flightkiosk.controllerTest;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.group2022103.flightkiosk.application.Application;
+import com.group2022103.flightkiosk.controller.TicketController;
+import com.group2022103.flightkiosk.mapper.TicketMapper;
+import com.group2022103.flightkiosk.vo.TicketBack;
+
+public class TicketControllerTest {
+	
+	private TicketMapper ticketMapper;
+	
+	@Test
+	public void test() {
+		Application.run();
+		ticketMapper = (TicketMapper) Application.context.getMapperConfig().getMappers().get(TicketMapper.class);
+		var para = new TicketBack();
+		para.setDocumentID("098765432112345678");
+		var ret = new TicketController().get(para).getTickets();
+		var retIds = new ArrayList<Integer>();
+		ret.forEach(e->retIds.add(e.getId()));
+		var cmp = List.of(10,4,11,9);
+		assertArrayEquals(cmp.toArray(), retIds.toArray());
+	}
+
+}
