1d4c45f3dd43e07a38a865184496b326906a8889
Wsq2019213431 <2461033848@qq.com>
2022-05-23 11:27:39 +0800

    111

--- src/test/java/com/group2022103/flightkiosk/RobotDemo.java
+++ src/test/java/com/group2022103/flightkiosk/RobotDemo.java
@@ -16,8 +16,9 @@ import java.awt.Robot;
 public class RobotDemo{
 
 public static void RobotRun(Robot robot){
-    robot.delay(1000);
-    System.out.println("Robot start");
+    //JOptionPane.showMessageDialog(null, "The demo is going to start,if any anomaly occurs,may the type of your screen matters,please turn to manual operation", "Caution!", JOptionPane.WARNING_MESSAGE);
+    System.out.println("Robot start...");
+    robot.delay(2000);
     robot.mouseMove(861,712);
     robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
     robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
@@ -49,7 +50,52 @@ public static void RobotRun(Robot robot){
     robot.mouseMove(1250,730);
     robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
     robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-
+    robot.delay(1000);
+    robot.mouseMove(730,500);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    robot.delay(1000);
+    robot.mouseMove(1250,730);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    robot.delay(1000);
+    robot.mouseMove(600,540);
+    for(int i=0;i<3;i++){
+    robot.delay(500);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+    robot.delay(1000);
+    robot.mouseMove(1250,730);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    robot.delay(1000);
+    robot.mouseMove(700,560);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    robot.delay(1000);
+    robot.mouseMove(1100,230);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    robot.delay(500);
+    robot.mouseMove(1250,730);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    robot.delay(1000);
+    robot.mouseMove(850,650);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    robot.delay(500);
+    robot.mouseMove(1000,650);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    robot.delay(1000);
+    robot.mouseMove(620,680);
+    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    robot.delay(5000);
+    JOptionPane.showMessageDialog(null, "Demo completed...", "Finish", JOptionPane.PLAIN_MESSAGE);
+    System.out.println("Demo complete...");
     
 }
 
@@ -66,6 +112,7 @@ public  static void main(String[] args){
     w.setVisible(true);
     w.setLocationRelativeTo(null);*/
     try{
+        JOptionPane.showMessageDialog(null, "The demo is going to start,if any anomaly occurs,may the type of your screen matters,please turn to manual operation", "Caution!", JOptionPane.WARNING_MESSAGE);
         Application.run();
     Robot robot=new Robot();
     RobotRun(robot);
